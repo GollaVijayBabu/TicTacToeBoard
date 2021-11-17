b55925e632233e5d88317a29f09672072456d3ca
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-11-16 21:26:25 -0800

    Added TicTacToeBoard

---
+++ UC_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeBoard
+{
+    class UC_1
+    {
+        //public int head =0;
+        //public int tail=1;
+        char[] arr = new char[10];
+        public void Board()
+        {
+            for (int i=0; i<=9; i++);
+            {
+                Console.WriteLine(arr[1]);
+            }
+        }
+    }
+}
